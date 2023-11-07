510304d3a9289188e8af90c7fae55ea5680527cf
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2023-11-06 20:01:43 -0500

    implement serial communication POLLING ONLY

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,20 @@
 #include "../lib/api/src/api.hpp"
 #include "../lib/wifi/src/wifi.hpp"
 
+// Objects
 wifi_connection wifi("LakeLaogai", "thereisnowifiinbasingse");
-
 api_lib api;
+
+// Global Variables
+const int debug = false;
 
 int connection_status;
 
+
+//  Prototyping
+void SerialEvent();
+
+// Setup and Loop
 void setup() {
   Serial.begin(115200);
 
@@ -21,9 +29,35 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Sending request");
+  // polling for serial input
+  SerialEvent();
+
+  if (debug) {
+    Serial.print("Connection status: ");
+    Serial.println(wifi.getStatus());
+    Serial.print("IP Address: ");
+    Serial.println(wifi.getIP());
+  }
+
   float data[] = {1.0, 2.0, 3.0};
   api.sendAmp(data, sizeof(data)/sizeof(data[0]));
-  // Serial.println(result);
   delay(1000);
 }
+
+// Functions
+void SerialEvent() {
+  while (Serial.available()) {
+    String inChar = Serial.readString();
+
+    if (inChar == "hello") {
+      Serial.println("hello, world!");
+    }
+    else if (inChar == "wifi_ip") {
+      Serial.print("IP Address: ");
+      Serial.println(wifi.getIP());
+    }
+    else {
+      Serial.println("Command not found");
+    }    
+  }
+} 
