b5daf458b12b7e7736727ca9d1224a20f6e575ed
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2023-11-24 14:47:10 -0500

    first implementation of the library

---
+++ lib/sensors/src/voltage_current.hpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+
+class VoltageSensor {
+  public:
+    /**
+     * @brief Construct a new Voltage Sensor object
+     * 
+     * @param pin 
+     */
+    VoltageSensor(int pin);
+    
+    /**
+     * @brief Setup the pin as input
+     */
+    void setup() {
+        pinMode(pin, INPUT);
+    };
+
+    /**
+     * @brief Read the raw value from the pin
+     * 
+     * @return float Value between 0 and 1023
+     */
+    float read() {
+        return analogRead(pin);
+    };
+
+    /**
+     * @brief Read the voltage from the pin
+     * 
+     * @return float 
+     */
+    float readVoltage() {
+        return read() / 1023.0; // real math : (5.0 * read() / 1023.0) / 5.0 
+    };
+
+  private:
+    int pin;
+};
+
+class CurrentSensor {
+    public:
+        /**
+         * @brief Construct a new Current Sensor object
+         * 
+         * @param pin 
+         */
+        CurrentSensor(int pin);
+        
+        /**
+         * @brief Setup the pin as input
+         */
+        void setup() {
+            pinMode(pin, INPUT);
+        };
+    
+        /**
+         * @brief Read the raw value from the pin
+         * 
+         * @return float Value between 0 and 1023
+         */
+        float read() {
+            return analogRead(pin);
+        };
+    
+        /**
+         * @brief Read the current from the pin
+         * 
+         * @return float 
+         */
+        float readCurrent(int param = 2.5) {
+            return (read() * (5.0 / 1023.0) - param) / 0.066;
+        };
+    
+    private:
+        int pin;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,72 @@
 #include <Arduino.h>
+#include "../lib/sensors/src/voltage_current.hpp"
 
-const int sensorPin = A0;
+const int battery_current_pin = A0;
+const int battery_voltage_pin = A1;
+
+const int solar_current_pin = A2;
+const int solar_voltage_pin = A3;
+
+const int 5v_current_pin = A4;
+const int 5v_voltage_pin = A5;
+
+const int 12v_current_pin = A6;
+const int 12v_voltage_pin = A7;
+
+CurrentSensor current_battery(battery_current_pin);
+VoltageSensor voltage_battery(battery_voltage_pin);
+
+CurrentSensor current_solar(solar_current_pin);
+VoltageSensor voltage_solar(solar_voltage_pin);
+
+CurrentSensor current_5v(5v_current_pin);
+VoltageSensor voltage_5v(5v_voltage_pin);
+
+CurrentSensor current_12v(12v_current_pin);
+VoltageSensor voltage_12v(12v_voltage_pin);
 
 void setup() {
-  pinMode(sensorPin, INPUT);
-
   Serial.begin(115200);
+
+  current_battery.setup();
+  voltage_battery.setup();
+
+  current_solar.setup();
+  voltage_solar.setup();
+
+  current_5v.setup();
+  voltage_5v.setup();
+
+  current_12v.setup();
+  voltage_12v.setup();
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);
+  Serial.print("Battery: ");
+  Serial.print(current_battery.readCurrent());
+  Serial.print("A, ");
+  Serial.print(voltage_battery.readVoltage());
+  Serial.println("V");
+
+  Serial.print("Solar: ");
+  Serial.print(current_solar.readCurrent());
+  Serial.print("A, ");
+  Serial.print(voltage_solar.readVoltage());
+  Serial.println("V");
+
+  Serial.print("5V: ");
+  Serial.print(current_5v.readCurrent());
+  Serial.print("A, ");
+  Serial.print(voltage_5v.readVoltage());
+  Serial.println("V");
+
+  Serial.print("12V: ");
+  Serial.print(current_12v.readCurrent());
+  Serial.print("A, ");
+  Serial.print(voltage_12v.readVoltage());
+  Serial.println("V");
+
+  Serial.println();
+
   delay(1000);
 }
