11d9425a5f65e21cdaac918fd2bd4cb17cc341ab
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2023-11-23 21:28:00 -0500

    add read sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
+const int sensorPin = A0;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(sensorPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(1000);
 }
